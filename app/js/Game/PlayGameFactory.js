65027f4608d24a163980ef1ce2434279da611a56
Make-It-Work <swhjenni@student.avans.nl>
2015-06-18 11:44:01 +0200

    Working last match

--- app/js/Game/PlayGameFactory.js
+++ app/js/Game/PlayGameFactory.js
@@ -1,4 +1,6 @@
-module.exports = function($http) {
+//WEL ASYNC
+
+module.exports = function($http, $q) {
 	var factory = {};
 
 	factory.gameTiles = [];
@@ -6,59 +8,80 @@ module.exports = function($http) {
 	factory.allTiles = [];
 
 	factory.loadGameTiles = function(gameId) {
-		var url = "https://mahjongmayhem.herokuapp.com/games/"+gameId+"/tiles?matched=false";
-		$http.get(url)
-		.success(function(response) {
-    		factory.gameTiles = response;
-    	});
-    	var url = "https://mahjongmayhem.herokuapp.com/games/"+gameId+"/tiles";
-		$http.get(url)
-		.success(function(response) {
-    		factory.allTiles = response;
-    	});
-		var url = "https://mahjongmayhem.herokuapp.com/games/"+gameId;
-		$http.get(url)
-		.success(function(response) {
-    		factory.players = response.players;
-    		factory.players.forEach(function(player) {
-    			player.lastMatch = [];
-    		});
-    	});
-    	var newUrl = "https://mahjongmayhem.herokuapp.com/games/" + gameId +"/tiles/matches";
-		$http.get(newUrl)
-		.success(function(response) {
-			response.forEach(function(match) {
-				factory.players.forEach(function(player) {
-					if(player.lastMatchFoundOn === undefined) {
-						player.lastMatchFoundOn = "1900-01-01T00:00:00.119Z";
-					}
-					if(player._id === match.match.foundBy) {
-						if (player.lastMatchFoundOn < match.match.foundOn) {
-							console.log("newer match");
-							player.lastMatchFoundOn = match.match.foundOn;
-							player.lastMatch[0] = match;
-							factory.allTiles.forEach(function(tile) {
-								// console.log(match);
-								// console.log(tile);
-								console.log('________');
-								console.log(tile.tile._id);
-								console.log(match.tile._id);
-
-								if(tile.tile._id == match.tile._id) {
-									console.log('filling lastMatch');
-									player.lastMatch[1] = match;
-								}
-							});
+		var unmatchedPromise = factory.getUnmatchedTiles(gameId);
+		var allTilesPromise = factory.getAllTiles(gameId);
+		var playersPromise = factory.getPlayers(gameId);
+		
+		$q.all([unmatchedPromise, allTilesPromise, playersPromise]).then(function() {
+			console.log('lm');
+	    	var newUrl = "https://mahjongmayhem.herokuapp.com/games/" + gameId +"/tiles/matches";
+			$http.get(newUrl)
+			.success(function(response) {
+				response.forEach(function(match) {
+					factory.players.forEach(function(player) {
+						if(player.lastMatchFoundOn === undefined) {
+							player.lastMatchFoundOn = "1900-01-01T00:00:00.119Z";
 						}
-					}
+						if(player._id === match.match.foundBy) {
+							if (player.lastMatchFoundOn < match.match.foundOn) {
+								console.log("newer match");
+								player.lastMatchFoundOn = match.match.foundOn;
+								player.lastMatch[0] = match;
+								factory.allTiles.forEach(function(tile) {
+									if(tile.tile._id == match.tile._id) {
+										console.log('filling lastMatch');
+										player.lastMatch[1] = match;
+									}
+								});
+							}
+						}
+					});
 				});
 			});
-		})
-		.error(function(response) {
-			console.log(response);
 		});
-
 	};
+
+	factory.getUnmatchedTiles = function(gameId) {
+		
+		return $q(function(resolve, reject) {
+			var url = "https://mahjongmayhem.herokuapp.com/games/"+gameId+"/tiles?matched=false";
+			$http.get(url)
+			.success(function(response) {
+				console.log('unmatched');
+	    		factory.gameTiles = response;
+	    		resolve(factory.gameTiles);
+	    	});
+		});		
+	};
+
+	factory.getAllTiles = function(gameId) {
+		
+		return $q(function(resolve, reject) {
+			var url = "https://mahjongmayhem.herokuapp.com/games/"+gameId+"/tiles";
+			$http.get(url)
+			.success(function(response) {
+				console.log('alltiles');
+	    		factory.allTiles = response;
+	    		resolve(factory.allTiles);
+	    	});
+		});
+	};
+
+	factory.getPlayers = function(gameId) {
+		
+		return $q(function(resolve, reject) {
+			var url = "https://mahjongmayhem.herokuapp.com/games/"+gameId;
+			$http.get(url)
+			.success(function(response) {
+				console.log('players');
+	    		factory.players = response.players;
+	    		factory.players.forEach(function(player) {
+	    			player.lastMatch = [];
+	    		});
+	    		resolve(factory.players);
+	    	});
+		});
+	}
 
 	return factory;
 };
